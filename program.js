8c8f94b7b1146ae416b6f737d00f9450020ce73d
szy0syz <szy0syz@gmail.com>
2016-05-21 15:49:39 +0800

    fixed

--- program.js
+++ program.js
@@ -1,1 +1,11 @@
-console.log("HELLO WORLD")
+var sum1 = 0;
+
+process.argv.forEach((val, index, array) => {
+  //console.log(`${index}: ${val}`);
+  if ((index >1)) {
+  	sum1 = Number(sum1) + Number(val);
+  };
+  
+});
+
+console.log(sum1);
