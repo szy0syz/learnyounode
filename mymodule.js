29a64753cd235eab67572b16990701d8c0bb14bc
szy0syz <szy0syz@gmail.com>
2016-05-23 23:20:02 +0800

    l6

---
+++ mymodule.js
@@ -0,0 +1,27 @@
+module.exports = function(dir,filter,callback){ 
+
+	// function bar (callback){
+	// 	foo(function (err,data){
+	// 		if(err)
+	// 			return callback(err) //early retrurn
+
+	// 		callback(null,data)
+	// 	})
+	// }
+
+	// function bar (callback){
+	// 	foo(function (err,data){
+	// 		if(err)
+	// 			return callback(err) //early retrurn
+
+	// 		callback(null,data)
+	// 	})
+	// }
+
+	console.log(dir,filter)
+	// console.log(process.argv[2])
+	// console.log(process.argv[3])
+	// console.log(process.argv[4])
+	// console.log(process.argv[5])
+
+}
---
+++ program_l6.js
@@ -0,0 +1,7 @@
+var my = require('./mymodule.js')
+
+var o = new my('dir','filter')
+
+// console.log(my.bar(function(err,data){
+// 	console.log("szy")
+// }))
