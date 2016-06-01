f70e3735800c623c1af2b94875cb7dab3203a7d5
szy0syz <185505508@qq.com>
2016-06-01 16:51:44 +0800

    l12 OK

--- program12.js
+++ program12.js
@@ -4,25 +4,17 @@ var http = require('http')
 var port = process.argv[2]
 
 var server = http.createServer(function(req,res){
+        
     if(req.method == "POST"){
         res.writeHead(200,{'content-type':'text/plain'})
-        var postdata = ""
-        req.addListener('data',function(chunk){
-            postdata += chunk
-        })
-        console.log(postdata)
-        // console.log("It's a "+ req.method)
-        // req.pipe(res)
-        //res.setEncoding('utf8')
-        // console.log(req)
-        // req.pipe(map(function(chunk){
-        //     return chunk.toString().split('').reverse().join('')
-        // })).pipe(res)
+        
+        req.pipe(map(function(chunk){  
+            return chunk.toString().toUpperCase()
+        })).pipe(res)
     }
     if(req.method == "GET"){
-        console.log('GET')
-    }
-    
+        console.log('GET')  
+    }  
 })
 
 server.listen(port)
