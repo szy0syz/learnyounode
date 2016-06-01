2cdc8618f7083731a1db09bd902f87f527438df0
szy0syz <szy0syz@gmail.com>
2016-06-02 00:09:41 +0800

    l13 test2

--- program13.js
+++ program13.js
@@ -1,15 +1,18 @@
 var http = require('http')
+var url = require('url')
 var port = process.argv[2]
 
 var server = http.createServer(function(req,res){
 	if(req.method != "GET") 
 		return console.log("Give me a Get")
 
+	res.writeHead(200,{'Content-Type':'application/json'})
+	
 	console.log(req.url)
 	//var date = new Date()
 
 	if (req.url == "/api/unixtime"){
-		console.log("unixtione OK")
+		console.log(Date.now())
 	}
 
 	if (req.url == "/api/parsetime") {
